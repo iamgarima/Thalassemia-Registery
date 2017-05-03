caf048b1332d84399e68d2bc15c69c1bc224a3fc
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-03 16:35:53 +0530

    Add styles to the data shown

--- public/app/components/PatientList.js
+++ public/app/components/PatientList.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import 'materialize-css/bin/materialize.css';
+import '../../assets/styles/main.css';
 
 const PatientList = () => {
   return (
-    <div className="row table-container">
-      <div className="col s8">
-        <table className="striped">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th>D.O.B</th>
-                <th>Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Alvin Eclair</td>
-                <td>08/05/1990</td>
-                <td>A</td>
-              </tr>
-            </tbody>
-          </table>    
+    <div className="row data-container">
+      <div className="col s10">
+        <div className="row header-row">
+          <ul>
+            <li className="col s1"></li>
+            <li className="col s5">Name</li>
+            <li className="col s4">D.O.B</li>
+            <li className="col s2">Type</li>
+          </ul>
+        </div>  
+        <div className="row content-row">
+          <ul>
+            <li className="col s1">1</li>
+            <li className="col s5">Alvin Eclair</li>
+            <li className="col s4">09/07/1990</li>
+            <li className="col s2">A</li>
+          </ul>
+        </div>     
       </div>
     </div>  
   )
