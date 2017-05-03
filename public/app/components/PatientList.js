49e390908eac40226f15785b669314e9a63dff2f
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-03 14:39:07 +0530

    Update component 'PatientList' to show list of patients in table Update webpack config to bundle css files

--- public/app/components/PatientList.js
+++ public/app/components/PatientList.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import 'materialize-css/bin/materialize.css';
 
 const PatientList = () => {
   return (
-    <h1>List of Patients</h1>
+    <div className="row table-container">
+      <div className="col s8">
+        <table className="striped">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>D.O.B</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Alvin Eclair</td>
+                <td>08/05/1990</td>
+                <td>A</td>
+              </tr>
+            </tbody>
+          </table>    
+      </div>
+    </div>  
   )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,17 @@ const config = {
         query: {
           presets: ['react', 'es2015', 'stage-3']
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader : 'file-loader'
       }
     ]
   },
