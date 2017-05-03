1a504f0e6b0964fd3e48e463c8e3653612616e5b
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-04 02:48:50 +0530

    Link 'Add Patient' to '/addpatient' route

--- public/app/components/Header.js
+++ public/app/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import 'materialize-css/bin/materialize.css';
 import '../../assets/styles/main.css';
 
@@ -8,13 +9,15 @@ class Header extends Component {
       <div className="row header-row">
         <div className="col s10 header-col">
           <div className="row">
-            <div className="col s1">
-              <i className="material-icons">add</i>
-            </div>
-            <div className="col s9 text-container">
-              <span className="add-text">ADD</span>
-              <span className="patient-text"> Patient</span>
-            </div>
+            <Link to={`/addpatient`}>
+              <div className="col s1">
+                <i className="material-icons">add</i>
+              </div>
+              <div className="col s9 text-container">
+                <span className="add-text">ADD</span>
+                <span className="patient-text"> Patient</span>
+              </div>
+            </Link>
             <div className="col s12 sub-heading">
               <p>You can add patients sufferring from thalassemia to provide them easy access of blood.</p>
             </div>
