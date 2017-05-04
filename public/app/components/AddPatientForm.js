02b8acc15e85d539f2fcc979458f94805fa5abe6
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-04 14:07:53 +0530

    Update component 'AddPatientForm' to send patient data to the server on clicking 'Submit' button

--- public/app/components/AddPatientForm.js
+++ public/app/components/AddPatientForm.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import 'materialize-css/bin/materialize.css';
 import 'materialize-css/bin/materialize.js';
 import '../../assets/styles/main.css';
+import { addPatient } from '../actions/patients';
 
 class AddPatientForm extends Component {
+  handleClick() {
+    const patient = {
+      fullName: this.fullName.value,
+      dob: this.dob.value,
+      diseaseType: this.diseaseType.value
+    };
+    this.props.addPatient(patient);
+  }
+
   render() {
     return (
       <div className="row addpatient-container">
@@ -13,29 +24,37 @@ class AddPatientForm extends Component {
         <form className="col s12">
           <div className="row">
             <div className="input-field col s8">
-              <input id="full_name" type="text" className="validate" />
-              <label for="full_name">Full Name</label>
+              <input ref={input => this.fullName = input} type="text" className="validate" />
+              <label>Full Name</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s8">
-              <input id="date_of_birth" type="text" className="validate" />
-              <label for="date_of_birth">Date of birth</label>
+              <input ref={input => this.dob = input} type="text" className="validate" />
+              <label>Date of birth</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s8">
-              <input id="disease_type" type="text" className="validate" />
-              <label for="disease_type">Disease Type</label>
+              <input ref={input => this.diseaseType = input} type="text" className="validate" />
+              <label>Disease Type</label>
             </div>
           </div>
         </form>
         <div className="col s12">
-          <a className="waves-effect waves-light btn">Submit</a>
+          <button className="waves-effect waves-light btn" onClick={() => this.handleClick()}>Submit</button>
         </div>
       </div>          
     )
   }
 }
 
-export default AddPatientForm;
+const mapDispatchToProps = dispatch => {
+  return {
+    addPatient: (patient) => {
+      dispatch(addPatient(patient))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AddPatientForm);
