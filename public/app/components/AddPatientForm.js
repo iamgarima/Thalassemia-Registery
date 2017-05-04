731afc332b07657bc885756f5bb74e9ef797e6b2
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-04 12:32:47 +0530

    Add and style component 'AddPatientForm' contain form for the user to fill patient details in order to add him to the record

--- public/app/components/AddPatientForm.js
+++ public/app/components/AddPatientForm.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import 'materialize-css/bin/materialize.css';
+import 'materialize-css/bin/materialize.js';
+import '../../assets/styles/main.css';
 
 class AddPatientForm extends Component {
   render() {
     return (
-      <h1>Enter Details</h1>
+      <div className="row addpatient-container">
+        <div className="col s12">
+          <h2>Details</h2>
+        </div>
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s8">
+              <input id="full_name" type="text" className="validate" />
+              <label for="full_name">Full Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s8">
+              <input id="date_of_birth" type="text" className="validate" />
+              <label for="date_of_birth">Date of birth</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s8">
+              <input id="disease_type" type="text" className="validate" />
+              <label for="disease_type">Disease Type</label>
+            </div>
+          </div>
+        </form>
+        <div className="col s12">
+          <a className="waves-effect waves-light btn">Submit</a>
+        </div>
+      </div>          
     )
   }
 }
