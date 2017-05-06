4f64753ab6df2f2bac6212c2562caff0a2ca0eaa
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-06 19:59:13 +0530

    Add model for users add function  - to insert a user to the user table  - to get all the users  - to get a user based on user email-id

---
+++ src/models/User.js
@@ -0,0 +1,41 @@
+import { tables } from '../db/database';
+
+function User(userDetails) {
+  this.emailId = userDetails.emailId;
+  this.password = userDetails.password;
+}
+
+const insert = (user, cb) => {
+  tables.User
+    .sync({force: false})
+    .then(() => tables.User.create(user).then((user) => {
+      cb(user);
+    }))
+}
+
+const getAll = (cb) => {
+  tables.User
+    .sync({force: false})
+    .then(() => tables.User.findAll().then((users) => {
+      cb(users);
+    }))
+}
+
+const checkUser = (user, cb) => {
+  tables.User
+    .sync({force: false})
+    .then(() => tables.User.findAll({
+      where: {
+        emailId: user.emailId 
+      }
+    }).then((users) => {
+      cb(users[0].dataValues);
+    }))
+} 
+
+module.exports = {
+  User,
+  insert,
+  getAll,
+  checkUser
+}
