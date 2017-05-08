1d6d4f84a83b66890d6637eb654b7f91a7ccd832
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-08 18:17:47 +0530

    Add check for -> only admin can add and view users
    update admin controller functions

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,16 +1,55 @@
-import { User, insert, getAll } from '../models/User';
+import passport from 'passport';
+import passportJWT from 'passport-jwt';
+import config from '../../config/config.json';
+import { User, insert, getAll, checkUser } from '../models/User';
 
-exports.addUser = (req, res) => {
-  const userDetails = new User(req.body);
-  insert(userDetails, (user) => {
-    if(user) res.send(user)
-    else res.status(500).send('ERROR')  
-  })
+const jwtOptions = {}
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
+jwtOptions.secretOrKey = config.secretOrKey;
+
+exports.addUser = (req, res, next) => {
+  passport.authenticate('jwt', { session: false, failureFlash: 'Unauthorised access' }, (val, user, jwt_payload) => {
+    if(user) {
+      if(user.isAdmin === true) {
+        const userDetails = new User(req.body);
+        insert(userDetails, (user) => {
+          if(user) res.send(user)
+          else res.status(500).send('ERROR')  
+        })
+      }
+    } else {
+      res.send('Unauthorised access');
+    }
+  })(req, res, next);
 }
 
-exports.getUsers = (req, res) => {
-  getAll((users) => {
-    if(users) res.send(users)
-    else res.status(500).send('ERROR')
-  })
+exports.getUsers = (req, res, next) => {
+  passport.authenticate('jwt', { session: false, failureFlash: 'Unauthorised access' }, (val, user, jwt_payload) => {
+    if(user) {
+      if(user.isAdmin === true) {
+        getAll((users) => {
+          if(users) res.send(users)
+          else res.status(500).send('ERROR')
+        })
+      }
+    } else {
+      res.send('Unauthorised access');
+    }
+  })(req, res, next);
 }
+
+const strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  checkUser(jwt_payload, (user) => {
+    if (user) {
+      next(null, user, jwt_payload);
+    } else {
+      next(null, false);
+    }
+  })
+});
+
+passport.use(strategy);
