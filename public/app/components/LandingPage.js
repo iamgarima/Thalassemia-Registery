0015a3aa5c07384609d5f4b1d77aa37c94f75d23
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-09 16:18:55 +0530

    Add route and initiate component for landing page

---
+++ public/app/components/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <h1>I am the LANDING page</h1>
+  )
+}
+
+export default LandingPage;
--- public/app/routes/index.js
+++ public/app/routes/index.js
@@ -8,13 +8,15 @@ import {
 import App from '../components/App';
 import PatientList from '../components/PatientList';
 import AddPatientForm from '../components/AddPatientForm';
+import LandingPage from '../components/LandingPage';
 
 
 const Routes = () => {
 	return (
     <Router history={browserHistory}> 
 		  <Route path="/" component={App}>
-        <IndexRedirect to="/patients" />
+        <IndexRedirect to="/thalassemia_registry" />
+        <Route path="thalassemia_registry" component={LandingPage} /> 
         <Route path="patients" component={PatientList} />
         <Route path="addpatient" component={AddPatientForm} />
       </Route>
