68a173f7b4a5d2015b590a9b68b2c64f8ebb3488
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-10 01:30:53 +0530

    Update action for user password set to not dispatch payload for user password state update

--- public/app/actions/users.js
+++ public/app/actions/users.js
@@ -4,18 +4,11 @@ import {
   USER_PASSWORD_ADDED
 } from './types.js';
 
-
 export const setUser = (user) => {
   return dispatch => {
     axios
       .post('api/v1/join', user)
       .then(res => {
-        dispatch({
-          type: USER_PASSWORD_ADDED,
-          payload: {
-            users: res.data
-          }
-        })
         browserHistory.push(`/patients`);
       })
       .catch(err => {
