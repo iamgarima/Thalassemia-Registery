66a72b08f28f74567cb217546a399befdf2e10d5
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-09 16:31:15 +0530

    Update component 'LandingPage'

--- public/app/components/LandingPage.js
+++ public/app/components/LandingPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const LandingPage = () => {
   return (
-    <h1>I am the LANDING page</h1>
+    <div>
+      <Link to={`/admin`}>
+        <p>Register Yourself</p>
+      </Link>
+      <Link to={`/join`}>
+        <p>Join Hospital</p>
+      </Link>
+      <Link to={`/login`}>
+        <p>Login</p>
+      </Link>
+    </div>  
   )
 }
 
