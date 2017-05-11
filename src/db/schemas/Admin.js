a24a05f46048ec1cc363844f0f40c1b4a638ea6a
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-11 17:01:23 +0530

    Update adminId type to Integer in admins table

--- src/db/schemas/Admin.js
+++ src/db/schemas/Admin.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize';
 export default (sequelize) => {
   const Admin = sequelize.define('admin', {
     adminId: {
-      type: Sequelize.STRING
+      type: Sequelize.INTEGER
     },
     usersList: {
       type: Sequelize.ARRAY(Sequelize.INTEGER)
