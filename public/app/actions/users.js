bcf943dddd627ddbc064b1225577951e26e15554
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-12 18:12:35 +0530

    Add user actions to send request to server to add and get all users

--- public/app/actions/users.js
+++ public/app/actions/users.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 import {
-  USER_PASSWORD_ADDED
+  USER_PASSWORD_ADDED,
+  USERS_AVAILABLE,
+  USER_ADDED
 } from './types.js';
 
 export const setUser = (user) => {
@@ -16,4 +18,41 @@ export const setUser = (user) => {
         alert('Password not added! Something went wrong!!');
       })
   }    
+}
+
+export const getUsers = () => {
+  return dispatch => {
+    axios
+    .get(`/api/v1/users`)
+    .then(res => {
+      dispatch({
+        type: USERS_AVAILABLE,
+        payload: {
+          users: res.data
+        }
+      })
+    })
+    .catch(err => {
+      alert('Request failed!!');
+    })
+  }
+}
+
+export const addUser = (user) => {
+  return dispatch => {
+    axios
+    .post(`/api/v1/users`, user)
+    .then(res => {
+      dispatch({
+        type: USER_ADDED,
+        payload: {
+          user: res.data
+        } 
+      })
+      browserHistory.push('/users');
+    })
+    .catch(err => {
+      alert('Request failed!!');
+    })
+  }
 }
