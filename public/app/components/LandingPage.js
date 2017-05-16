f9852b5d11d46a231153f98fe28511d603639b7c
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-16 11:17:00 +0530

    Add styles to landing page

--- public/app/components/LandingPage.js
+++ public/app/components/LandingPage.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router';
+import 'materialize-css/bin/materialize.css';
+import '../../assets/styles/main.css';
 
 const LandingPage = () => {
   return (
-    <div>
-      <Link to={`/admin`}>
-        <p>Register Yourself</p>
-      </Link>
-      <Link to={`/join`}>
-        <p>Join Hospital</p>
-      </Link>
-      <Link to={`/login`}>
-        <p>Login</p>
-      </Link>
-    </div>  
+    <div className="outer-image-container">
+      <div className="active-container">
+        <h5>Thalassemia Registry</h5>
+        <Link to={`/admin`}>
+          <p>Register Yourself</p>
+        </Link>
+        <Link to={`/join`}>
+          <p>Join Hospital</p>
+        </Link>
+        <Link to={`/login`}>
+          <p>Login</p>
+        </Link>
+      </div>
+    </div>
   )
 }
 
